15917b2af408d72fb46e9191710f4603e575c300
JoshuaLChung <jchung20@horizon.csueastbay.edu>
2022-11-17 15:17:34 -0800

    Initial logic for login request between client and server.

--- src/Client.java
+++ src/Client.java
@@ -3,63 +3,49 @@ import java.io.*;
 import java.util.Scanner;
 public class Client {
 	private Socket socket = null;
-    private InputStreamReader in = null;
-    private OutputStreamWriter out = null;
-    private BufferedReader reader = null;
-    private BufferedWriter writer = null;
+	private ObjectInputStream objectIn = null;
+	private ObjectOutputStream objectOut = null;
 	
 	public Client(String address, int port) {
 		try {
 			socket = new Socket(address, port);
 			System.out.println("Client connected");
 			
-			in = new InputStreamReader(socket.getInputStream());
-			out = new OutputStreamWriter(socket.getOutputStream());
-			
-			reader = new BufferedReader(in);
-			writer = new BufferedWriter(out);
+			objectOut = new ObjectOutputStream(socket.getOutputStream());
+			objectIn = new ObjectInputStream(socket.getInputStream());
 			
 			Scanner scanner = new Scanner(System.in);
 			
+			// Login object created for client login request
 			String accNum = "";
 			String pin = "";
-			
 			System.out.println("Enter card number: ");
 			accNum = scanner.nextLine();
-			writer.write(accNum);
-			writer.newLine();
-			writer.flush();
-			
 			System.out.println("Enter PIN: ");
 			pin = scanner.nextLine();
-			writer.write(pin);
-			writer.newLine();
-			writer.flush();
+			Login customerLogin = new Login(accNum, pin);
+			objectOut.writeObject(customerLogin);
 			
-			System.out.println("Server: " + reader.readLine());
+			// Server should return customers accounts to use for bank gui
+			
 		}
 		catch(IOException e) {
-			System.out.println(e);
-		}
-		try {
-			if (socket != null) {
-				socket.close();
+			e.printStackTrace();
+		} finally {
+			try {
+				if (objectIn != null) {
+					objectIn.close();
+				}
+				if (objectOut != null) {
+					objectOut.close();
+				}
+				if (socket != null) {
+					socket.close();
+				}
 			}
-			if (in != null) {
-				in.close();
+			catch (IOException e) {
+				e.printStackTrace();
 			}
-			if (out != null) {
-				out.close();
-			}
-			if (writer != null) {
-				writer.close();
-			}
-			if (reader != null) {
-				reader.close();
-			}
-		}
-		catch (IOException e) {
-			System.out.println(e);
 		}
 	}
 	
