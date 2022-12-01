f05876bb3dfb5e76a14621614f3fccbf0ba861de
joshualchung <90055401+joshualchung@users.noreply.github.com>
2022-11-30 22:29:38 -0800

    Updated AllTests with all unit tests

--- src/testing/AllTests.java
+++ src/testing/AllTests.java
@@ -4,5 +4,5 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({TransactionTests.class, TellerLoginTests.class, RequestTests.class, ServerTests.class})
+@SelectClasses({TransactionTests.class, TellerLoginTests.class, RequestTests.class, ServerTests.class, CustomerTest.class, LoginTest.class, AccountTest.class})
 public class AllTests {}
